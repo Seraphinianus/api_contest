12511de00d1eb6aefc4d0b65bff34513fa02eb8e
Seraphinianus <sebastian.dima@stud.ubbcluj.ro>
2023-07-22 17:10:59 +0300

    Cleaned up even more

--- my-invoice-app/src/App.js
+++ my-invoice-app/src/App.js
@@ -2,17 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import Setup from '../src/boilerplates/1-first-request';
 import Setup2 from '../src/boilerplates/2-headers';
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Box(props) {
+  const mesh = useRef();
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (
+     <mesh {...props} ref={mesh}>
+        <boxGeometry args={[3, 3, 3]} />
+        <meshStandardMaterial color={"orange"} />
+     </mesh>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>sal ba</p>
         <Setup />
         <Setup2 />
       </header>
     </div>
+
   );
 }
 
---
+++ my-invoice-app/src/components/3d-cube.js
@@ -0,0 +1,14 @@
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+function Box(props) {
+   const mesh = useRef();
+   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+   return (
+      <mesh {...props} ref={mesh}>
+         <boxGeometry args={[3, 3, 3]} />
+         <meshStandardMaterial color={"orange"} />
+      </mesh>
+   );
+}
+export default Box();
